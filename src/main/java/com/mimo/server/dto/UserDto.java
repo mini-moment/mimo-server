4c8f61c7674141b96e836ea67162a2038a8572f4
jaehan4707 <jahan4707@naver.com>
2024-04-27 21:08:23 +0900

    feat(#3): UserDto 수정

--- src/main/java/com/mimo/server/dto/UserDto.java
+++ src/main/java/com/mimo/server/dto/UserDto.java
@@ -1,19 +1,50 @@
 package com.mimo.server.dto;
 
 public class UserDto {
-	private int id;
-	private String username;
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
 
+    private int userId;
+    private String userName;
+    private String userContract;
+    private String accessToken;
+    private String refreshToken;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserContract() {
+        return userContract;
+    }
+
+    public void setUserContract(String userContract) {
+        this.userContract = userContract;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
 }
