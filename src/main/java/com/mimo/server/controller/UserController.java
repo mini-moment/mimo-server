e0f3cff58fed375cf6ef25ec90349c9338f0d83e
jaehan4707 <jahan4707@naver.com>
2024-05-01 20:55:42 +0900

    feat(#5): signUp 반환타입 변경

--- src/main/java/com/mimo/server/controller/UserController.java
+++ src/main/java/com/mimo/server/controller/UserController.java
@@ -2,24 +2,26 @@ package com.mimo.server.controller;
 
 import com.mimo.server.dto.UserDto;
 import com.mimo.server.service.UserService;
+import com.mimo.server.util.ApiUtil;
+import com.mimo.server.util.ApiUtil.ApiSuccessResult;
 import io.swagger.v3.oas.annotations.Operation;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Slf4j
+@ControllerAdvice
 public class UserController {
 
-    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     UserService service;
 
     @PostMapping("/signUp")
     @Operation(summary = "유저의 정보를 바탕으로 회원가입을 합니다.")
-    public boolean signUp(@RequestBody UserDto userDto) {
-        return service.signUp(userDto);
+    public ApiSuccessResult<Boolean> signUp(@RequestBody UserDto userDto) {
+        return ApiUtil.success(service.signUp(userDto));
     }
 
     @GetMapping("/getUser/{id}")
@@ -27,7 +29,7 @@ public class UserController {
     public UserDto getUser(@PathVariable int id) {
         try {
             UserDto user = service.getUser(id);
-            logger.debug("user : {}", user);
+            log.debug("user : {}", user);
             return user;
         } catch (Exception e) {
             throw e;
