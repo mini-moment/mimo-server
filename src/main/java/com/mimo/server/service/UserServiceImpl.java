0dff670daf96596e58547442100dfbffeb909127
jaehan4707 <jahan4707@naver.com>
2024-05-01 20:55:57 +0900

    feat(#5): service 계층에서 validation 검사

--- src/main/java/com/mimo/server/service/UserServiceImpl.java
+++ src/main/java/com/mimo/server/service/UserServiceImpl.java
@@ -1,12 +1,16 @@
 package com.mimo.server.service;
 
+import com.mimo.server.dao.UserDao;
+import com.mimo.server.dto.UserDto;
+import com.mimo.server.error.CustomException;
+import com.mimo.server.util.MybatisConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Service;
 
-import com.mimo.server.dao.UserDao;
-import com.mimo.server.dto.UserDto;
-import com.mimo.server.util.MybatisConfig;
+import static com.mimo.server.error.CustomErrorCode.INVALID_DATA_FORMAT;
 
+@Slf4j
 @Service
 public class UserServiceImpl implements UserService {
 
@@ -20,6 +24,9 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public boolean signUp(UserDto user) {
+        if (!user.validation()) {
+            throw new CustomException(INVALID_DATA_FORMAT);
+        }
         try (SqlSession session = MybatisConfig.getSqlSession();) {
             UserDao dao = session.getMapper(UserDao.class);
             return dao.signUp(user);
