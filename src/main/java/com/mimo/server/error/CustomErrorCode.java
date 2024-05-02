42cf7515679637e757508668d61e7803618f2e0c
jaehan4707 <jahan4707@naver.com>
2024-05-02 21:33:10 +0900

    feat(#5): CustomErrorCode httpStatus, statusCode 추가

--- src/main/java/com/mimo/server/error/CustomErrorCode.java
+++ src/main/java/com/mimo/server/error/CustomErrorCode.java
@@ -1,14 +1,29 @@
 package com.mimo.server.error;
 
-public enum CustomErrorCode {
-    INVALID_DATA_FORMAT("Invalid data format");
-    private final String message;
+import org.springframework.http.HttpStatus;
 
-    CustomErrorCode(String message) {
+public enum CustomErrorCode {
+    INVALID_DATA_FORMAT(ErrorMessage.INVALID_DATA_FORMAT, HttpStatus.BAD_REQUEST, 400);
+    private final String message;
+    private final HttpStatus httpStatus;
+
+    private final int statusCode;
+
+    CustomErrorCode(String message, HttpStatus httpStatus, int statusCode) {
         this.message = message;
+        this.httpStatus = httpStatus;
+        this.statusCode = statusCode;
     }
 
     public String getMessage() {
         return message;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
     }
 }
