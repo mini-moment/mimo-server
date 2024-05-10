9f9ba65ed3c0dd7bda3b1fd5dc317c9f17aa58df
pooya98 <pooya98@naver.com>
2024-05-11 00:04:36 +0900

    feat(#11): 비디오 업로드 커스텀에러 생성(상태코드 : 512(VIDEO_UPLOAD_SERVER_ERROR))

--- src/main/java/com/mimo/server/error/ErrorMessage.java
+++ src/main/java/com/mimo/server/error/ErrorMessage.java
@@ -3,4 +3,5 @@ package com.mimo.server.error;
 public class ErrorMessage {
 
     public static final String INVALID_DATA_FORMAT = "데이터 형식이 잘못 되었습니다.";
+    public static final String VIDEO_UPLOAD_SERVER_ERROR = "서버에서 비디오 파일 업로드 시, 에러가 발생하였습니다.";
 }
