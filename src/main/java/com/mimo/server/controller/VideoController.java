36111d5553ec3a4e1376eff061c1d5ef665a80c1
pooya98 <pooya98@naver.com>
2024-05-11 00:08:11 +0900

    feat(#11): VideoController 구현 (비디오파일 업로드, 비디오파일 다운로드, 비디오 스트리밍)

--- src/main/java/com/mimo/server/controller/VideoController.java
+++ src/main/java/com/mimo/server/controller/VideoController.java
@@ -1,43 +1,98 @@
 package com.mimo.server.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-import com.mimo.server.dto.UserDto;
-import com.mimo.server.dto.VideoDto;
-import com.mimo.server.service.UserService;
+import com.mimo.server.error.CustomErrorCode;
+import com.mimo.server.error.CustomException;
 import com.mimo.server.service.VideoService;
+import com.mimo.server.util.ApiUtil;
+import com.mimo.server.util.ApiUtil.ApiSuccessResult;
 
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
+@RequestMapping("/video")
+@RequiredArgsConstructor
 public class VideoController {
 	
-	@Autowired
-	VideoService service;
+	private final VideoService service;
+	private final String videoStoragePath = System.getProperty("user.dir").toString() + "/video";	// videoPath Directory Path
+	ServletContext servletContext;
 
+	@PostMapping("/upload")
+	public ApiSuccessResult<String> uploadVideoFile(
+			@RequestParam("video")
+			MultipartFile uploadFile, HttpServletRequest req){
+		
+		UUID uuid = UUID.randomUUID();
+		String filename = uuid.toString() + "_" + uploadFile.getOriginalFilename();
+		String filepath = videoStoragePath + "/" + filename;
+		File file = new File(filepath);
+		
+		try {
+            BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file));
+            bos.write(uploadFile.getBytes());
+            bos.close();
+        
+        } catch (Exception e) {
+        	throw new CustomException(CustomErrorCode.VIDEO_UPLOAD_SERVER_ERROR);
+        } 
+        return ApiUtil.success(filename);
+	}
 	
-	@GetMapping("/{videoId}/download")
-    public ResponseEntity<Resource> downloadVideo(@PathVariable Long videoId) {
-
-        Resource videoFile = service.loadVideo(videoId);
+	@GetMapping("/download")
+	public ResponseEntity<InputStreamResource> downloadFile(String url, HttpServletRequest req) throws Exception{
+        File file = new File(videoStoragePath + "/" + url);
+        InputStreamResource isr = new InputStreamResource(new FileInputStream(file));
         
-        // 적절한 헤더 설정
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=video.mp4");
-        
-        // 파일과 헤더를 포함한 ResponseEntity 반환
         return ResponseEntity.ok()
-                .headers(headers)
-                .contentType(MediaType.parseMediaType("video/mp4"))
-                .body(videoFile);
-    }
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + file.getName())
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .contentLength(file.length())
+                .body(isr);
+	}
+	
+	@GetMapping("display/{url}")
+	public ResponseEntity<?> display(@PathVariable String url, HttpServletRequest req) throws Exception{
+		String path = videoStoragePath + "/" + url;
+		Resource resource =  new FileSystemResource(path);
+		HttpHeaders httpHeaders = new HttpHeaders();
+        try {
+            Path filepath = Paths.get(path);
+            httpHeaders.add("Content-Type", Files.probeContentType(filepath));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        
+        return new ResponseEntity<>(resource, httpHeaders, HttpStatus.OK);
+	}
 }
 
 
