0ff42370ff42c4d895f98237be2b2d6e727fd907
jaehan4707 <jahan4707@naver.com>
2024-05-11 21:58:51 +0900

    feat(#10): PostDto 수정

--- src/main/java/com/mimo/server/dto/PostDto.java
+++ src/main/java/com/mimo/server/dto/PostDto.java
@@ -1,50 +1,22 @@
 package com.mimo.server.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
 public class PostDto {
-	private int id;
-	private String title;
-	private int userID;
-	private int videoID;
-	private String latitude;
-	private String longitude;
-	
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public int getUserID() {
-		return userID;
-	}
-	public void setUserID(int userID) {
-		this.userID = userID;
-	}
-	public int getVideoID() {
-		return videoID;
-	}
-	public void setVideoID(int videoID) {
-		this.videoID = videoID;
-	}
-	public String getLatitude() {
-		return latitude;
-	}
-	public void setLatitude(String latitude) {
-		this.latitude = latitude;
-	}
-	public String getLongitude() {
-		return longitude;
-	}
-	public void setLongitude(String longitude) {
-		this.longitude = longitude;
-	}
-	
-	
+
+    private int id;
+    @NonNull
+    private String title;
+    private int userId;
+    @NonNull
+    private String videoUrl;
+    private long latitude;
+    private long longitude;
+    private List<TagDto> tagList;
 }
