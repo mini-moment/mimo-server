d8ee5e62eeb696fab0e47fc582040dbda77aa52d
jaehan4707 <jahan4707@naver.com>
2024-06-12 11:44:30 +0900

    feat(#24): getUserById로 변경

--- src/main/java/com/mimo/server/dao/UserDao.java
+++ src/main/java/com/mimo/server/dao/UserDao.java
@@ -4,7 +4,7 @@ import com.mimo.server.dto.UserDto;
 
 public interface UserDao {
 
-    public UserDto getUserByAccessToken(int id);
+    public UserDto getUserById(int id);
 
     public boolean signUp(UserDto user);
 
