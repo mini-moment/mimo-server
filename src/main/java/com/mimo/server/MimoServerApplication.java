748c374bf37878accb658f378130165d3925ddc7
jaehan4707 <jahan4707@naver.com>
2024-06-16 16:17:52 +0900

    feat(#26):  썸네일 폴더 만들기 추가

--- src/main/java/com/mimo/server/MimoServerApplication.java
+++ src/main/java/com/mimo/server/MimoServerApplication.java
@@ -2,6 +2,10 @@
 package com.mimo.server;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -14,23 +18,39 @@ import lombok.extern.slf4j.Slf4j;
 @SpringBootApplication
 public class MimoServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MimoServerApplication.class, args);
-	}
-	
-	@Value("${path.videoStoragePath}")
-	private String videoStoragePath;
+    public static void main(String[] args) {
+        SpringApplication.run(MimoServerApplication.class, args);
+    }
 
-	@Bean
-	public boolean createVideoDirectory() {
-		File Folder = new File(videoStoragePath);
-		if (!Folder.exists()) {
-			try {
-				Folder.mkdir();
-			} catch (Exception e) {
-				e.getStackTrace();
-			}
-		}
-		return true;
-	}
+    @Value("${path.videoStoragePath}")
+    private String videoStoragePath;
+
+    @Value("${path.thumbnailStoragePath}")
+    private String thumbnailStoragePath;
+
+    @Bean
+    public boolean createVideoDirectory() {
+        File Folder = new File(videoStoragePath);
+        if (!Folder.exists()) {
+            try {
+                Folder.mkdir();
+            } catch (Exception e) {
+                e.getStackTrace();
+            }
+        }
+        return true;
+    }
+
+    @Bean
+    public boolean createThumbnailDirectory() {
+        Path path = Paths.get(thumbnailStoragePath);
+        try {
+            if (!Files.exists(path)) {
+                Files.createDirectory(path);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
 }
