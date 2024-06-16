9c40932521abda246aaa2ae4ec25baf20c727a05
jaehan4707 <jahan4707@naver.com>
2024-06-16 16:21:46 +0900

    feat(#26): 썸네일 저장 썸네일 조회 추가

--- src/main/java/com/mimo/server/service/PostServiceImpl.java
+++ src/main/java/com/mimo/server/service/PostServiceImpl.java
@@ -1,19 +1,31 @@
 package com.mimo.server.service;
 
-import java.util.List;
-
-import org.apache.ibatis.session.SqlSession;
-import org.springframework.stereotype.Service;
-
 import com.mimo.server.dao.PostDao;
 import com.mimo.server.dto.PostDto;
+import com.mimo.server.error.CustomErrorCode;
+import com.mimo.server.error.CustomException;
 import com.mimo.server.util.MybatisConfig;
-
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @Slf4j
 public class PostServiceImpl implements PostService {
+
+    @Value("${path.thumbnailStoragePath}")
+    private String thumbnailStoragePath;
+
     @Override
     public PostDto insertPost(PostDto post) {
         try (SqlSession session = MybatisConfig.getSqlSession();) {
@@ -23,11 +35,31 @@ public class PostServiceImpl implements PostService {
         }
     }
 
-	@Override
-	public List<PostDto> getPostsByIds(int[] ids) {
-		try (SqlSession session = MybatisConfig.getSqlSession();) {
+    @Override
+    public List<PostDto> getPostsByIds(int[] ids) {
+        try (SqlSession session = MybatisConfig.getSqlSession();) {
             PostDao dao = session.getMapper(PostDao.class);
             return dao.searchPostsByIds(ids);
         }
-	}
+    }
+
+    @Override
+    public String saveThumbnail(MultipartFile thumbnail) {
+        UUID uuid = UUID.randomUUID();
+        String filename = uuid + "_" + thumbnail.getOriginalFilename();
+        String filePath = thumbnailStoragePath + "/" + filename;
+        File file = new File(filePath);
+        try (InputStream inputStream = thumbnail.getInputStream()) {
+            Files.copy(inputStream, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            throw new CustomException(CustomErrorCode.IMAGE_UPLOAD_SERVER_ERROR);
+        }
+        return filename;
+    }
+
+    @Override
+    public Resource getThumbnail(String url) {
+        String path = thumbnailStoragePath + "/" + url;
+        return new FileSystemResource(path);
+    }
 }
