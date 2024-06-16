5e93b974f8085cc2ff09390945cd96411430ba67
jaehan4707 <jahan4707@naver.com>
2024-06-16 16:18:41 +0900

    feat(#26):  썸네일 저장 커스텀 에러 구현

--- src/main/java/com/mimo/server/error/CustomErrorCode.java
+++ src/main/java/com/mimo/server/error/CustomErrorCode.java
@@ -4,7 +4,9 @@ import org.springframework.http.HttpStatus;
 
 public enum CustomErrorCode {
     INVALID_DATA_FORMAT(ErrorMessage.INVALID_DATA_FORMAT, HttpStatus.BAD_REQUEST, 400),
-	VIDEO_UPLOAD_SERVER_ERROR(ErrorMessage.VIDEO_UPLOAD_SERVER_ERROR, HttpStatus.INTERNAL_SERVER_ERROR, 512);
+	VIDEO_UPLOAD_SERVER_ERROR(ErrorMessage.VIDEO_UPLOAD_SERVER_ERROR, HttpStatus.INTERNAL_SERVER_ERROR, 512),
+    IMAGE_UPLOAD_SERVER_ERROR(ErrorMessage.IMAGE_UPLOAD_SERVER_ERROR, HttpStatus.INTERNAL_SERVER_ERROR,513);
+
     private final String message;
     private final HttpStatus httpStatus;
 
