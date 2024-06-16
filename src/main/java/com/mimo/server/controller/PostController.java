73667a27df110e42308f71728259ef8be0a39c45
jaehan4707 <jahan4707@naver.com>
2024-06-16 16:22:54 +0900

    feat(#26): PostController getThumbnail Api 추가

--- src/main/java/com/mimo/server/controller/PostController.java
+++ src/main/java/com/mimo/server/controller/PostController.java
@@ -2,12 +2,11 @@ package com.mimo.server.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import com.mimo.server.dto.HashTagDto;
 import com.mimo.server.dto.PostDto;
@@ -21,6 +20,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.multipart.MultipartFile;
 
 
 @RestController
@@ -36,19 +36,24 @@ public class PostController {
 
     @PostMapping("insert")
     @Operation(summary = "포스트를 작성합니다.")
-    public ApiUtil.ApiSuccessResult<Boolean> insertPost(@RequestBody PostDto post, HttpServletRequest request) {
+    public ApiUtil.ApiSuccessResult<Boolean> insertPost(
+            @RequestPart("post") PostDto post,
+            @RequestPart("thumbnail") MultipartFile thumbnail,
+            HttpServletRequest request) {
         String authorizationHeader = request.getHeader("Authorization");
         UserDto user = userService.getUserByAccessToken(authorizationHeader);
         int markerId = post.getMarkerId();
         if (markerId == 0) {
             markerId = 1;
         }
+        String thumbnailUrl = postService.saveThumbnail(thumbnail);
         PostDto dto = new PostDto(post.getId(),
                 post.getTitle(),
                 user.getId(),
                 post.getVideoUrl(),
                 markerId,
-                post.getTagList()
+                post.getTagList(),
+                thumbnailUrl
         );
         int postId = postService.insertPost(dto).getId();
         List<HashTagDto> hashTagList = dto.getTagList().stream()
@@ -62,15 +67,24 @@ public class PostController {
         hashTagService.insertHashTagList(hashTagList);
         return ApiUtil.success(true);
     }
-    
+
     @GetMapping("posts")
     @Operation(summary = "전달받은 Post ID 리스트에 대한 Post 리스트를 반환합니다.")
-    public ApiUtil.ApiSuccessResult<List<PostDto>> getPosts(@RequestBody int[] ids){
-    	try {
-            List<PostDto> posts = postService.getPostsByIds(ids);
-            return ApiUtil.success(posts);
-        } catch (Exception e) {
-            throw e;
-        }
+    public ApiUtil.ApiSuccessResult<List<PostDto>> getPosts(@RequestBody int[] ids) {
+        List<PostDto> posts = postService.getPostsByIds(ids);
+        return ApiUtil.success(posts);
     }
+
+    @GetMapping("thumbnail/{url}")
+    @Operation(summary = "경로에 해당하는 Post의 썸네일을 반환합니다.")
+    public ResponseEntity<Resource> display(@PathVariable String url) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        if (url.contains("png")) {
+            httpHeaders.add(HttpHeaders.CONTENT_TYPE, "image/png");
+        } else if (url.contains("jpeg")) {
+            httpHeaders.add(HttpHeaders.CONTENT_TYPE, "image/jpeg");
+        }
+        return new ResponseEntity<>(postService.getThumbnail(url), httpHeaders, HttpStatus.OK);
+    }
+
 }
