53a1e50e1a27f3ee07352e44f14d9af14f423e7a
jaehan4707 <jahan4707@naver.com>
2024-07-16 23:07:53 +0900

    feat(#37): 유저가 있을 경우 updateUser 구현

--- src/main/java/com/mimo/server/dao/UserDao.java
+++ src/main/java/com/mimo/server/dao/UserDao.java
@@ -6,7 +6,9 @@ public interface UserDao {
 
     public UserDto getUserById(int id);
 
-    public boolean signUp(UserDto user);
+    public int signUp(UserDto user);
 
     public UserDto getUserByAccessToken(String accessToken);
+
+    public int updateUser(UserDto user);
 }
